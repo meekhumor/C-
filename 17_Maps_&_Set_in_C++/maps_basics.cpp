c1c6d24119c68c35ece61e7b2ff583f703f41417
Om Mukherjee <om17092004@gmail.com>
2024-10-29 10:10:08 +0530

    C++

---
+++ 17_Maps_&_Set_in_C++/maps_basics.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    system("cls");
+    unordered_map<string, int> m;
+    pair<string, int> p1;
+    p1.first = "Om";
+    p1.second = 76;
+    m.insert(p1);
+
+    pair<string, int> p2;
+    p2.first = "Hello";
+    p2.second = 23;
+    m.insert(p2);
+
+    // another way to insert
+    m["Lokesh"] = 34;
+    m["Rahul"] = 35;
+    
+    for (pair<string,int> p : m){
+        cout<<p.first<<" "<<p.second<<endl;
+    }
+    return 0;
+}
---
+++ 17_Maps_&_Set_in_C++/sets_basics.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    system("cls");
+    unordered_set<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+
+    for (int ele : s){
+        cout << ele << " ";
+    }
+    
+    int target = 4;
+    // s.find() -> it searches in the set, and if it is not found then it returns the last element
+    if(s.find(target)!= s.end()){ // Target exists
+        cout << "Target exists" << endl;
+    } 
+    
+    return 0;
+}
