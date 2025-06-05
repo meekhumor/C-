6180cf9e543ff0e96014f33e68dfbea0f9c87564
meekhumor <om17092004@gmail.com>
2025-06-05 20:56:29 +0530

    Refactor input/output loops for consistency and readability in cyclic sort and prefix sum algorithms

--- 07_Sorting_in_C++/07_Cyclic_sort.cpp
+++ 07_Sorting_in_C++/07_Cyclic_sort.cpp
@@ -13,28 +13,19 @@ int main()
     int arr[n];
     cout<<"Enter "<<n<<" elements of array : ";
 
-    for (int i = 0; i < n; i++)
-    {
-        cin>>arr[i];
-    }
+    for (int i = 0; i < n; i++) cin>>arr[i];
+    
 
-    for (int i = 0; i < n; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
+    for (int i = 0; i < n; i++) cout<<arr[i]<<" ";
     cout<<endl;
     
     int i = 0;
-    while(i<n)
-    {
+    while(i<n){
         int crtidx = arr[i] - 1;
         if(i == crtidx) i++;
         else swap(arr[i], arr[crtidx]);
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
+    for (int i = 0; i < n; i++) cout<<arr[i]<<" ";
     return 0;
 }
--- 10_Special_Algorithm/01_Prefix_sum.cpp
+++ 10_Special_Algorithm/01_Prefix_sum.cpp
@@ -10,22 +10,17 @@ int main()
 
     vector<int> nums; // {1,4,5,3,2,7,6}
     cout << "Enter " << n << " elements of array: ";
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         int x;
         cin >> x;
         nums.push_back(x);
     }
 
-    for (int i = 1; i < nums.size(); i++) // prefix sum array: {1,5,10,13,15,22,28}
-    {
-        nums[i] += nums[i - 1];
-    }
+    for (int i = 1; i < nums.size(); i++) nums[i] += nums[i - 1];// prefix sum array: {1,5,10,13,15,22,28}
 
-    for (int i = 0; i < nums.size(); i++)
-    {
-        cout << nums[i] << " ";
-    }
+    for (int i =n-2; i >= 0; i--) nums[i] += nums[i + 1];// suffix sum array: {28, 27, 23, 18, 15, 13, 6}
 
+
+    for (int i = 0; i < nums.size(); i++) cout << nums[i] << " ";
     return 0;
 }
